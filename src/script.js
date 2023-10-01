38ea3faf37eb57db6efd01b98506b9e5767aa761
Ana SL <ana.wk.lsl@gmail.com>
2023-10-01 20:23:28 +0100

    gallery

--- src/script.js
+++ src/script.js
@@ -1,12 +1,70 @@
 const paintings = [
     {
         imageSrc: 'src/img/1.jpeg',
-        description: 'Description for Painting 1',
+        title: 'Painting 1',
+        description: 'Se eu quisesse dizia que era um manjerico',
     },
     {
         imageSrc: 'src/img/2.jpeg',
-        description: 'Description for Painting 2',
+        description: 'Na próxima primavera haverá flores com cores...',
     },
+    {
+        imageSrc: 'src/img/3.jpeg',
+        description: 'Quando descansar vou subir as escadas e espreitar...',
+    },
+    {
+        imageSrc: 'src/img/3.png',
+        description: 'Fato novo para fim de estação...',
+    },
+    {
+        imageSrc: 'src/img/11.jpeg',
+        description: 'Título em branco',
+    },
+    {
+        imageSrc: 'src/img/12.jpeg',
+        description: 'Há papaiolas debaixo do verde',
+    },
+    {
+        imageSrc: 'src/img/12.png',
+        description: 'À espera do outono para mudar de roupa...',
+    },
+    {
+        imageSrc: 'src/img/21.png',
+        description: 'Sim...são manjericos',
+    },
+    {
+        imageSrc: 'src/img/31.jpeg',
+        description: 'Era uma vez um portão desnecessário...',
+    },
+    {
+        imageSrc: 'src/img/32.jpeg',
+        description: 'Campo de trigo',
+    },
+    {
+        imageSrc: 'src/img/33.jpeg',
+        description: 'Sim...sim...é uma ponte...',
+    },
+    {
+        imageSrc: 'src/img/34.jpeg',
+        description: 'Sonhei com um deserto...ou perto!',
+    },
+    {
+        imageSrc: 'src/img/41.jpeg',
+        description: 'Título em branco',
+    },
+    {
+        imageSrc: 'src/img/42.jpeg',
+        description: 'Título em branco',
+    },
+    {
+        imageSrc: 'src/img/43.jpeg',
+        description: 'Título em branco',
+    },
+    {
+        imageSrc: 'src/img/44.jpeg',
+        description: 'Título em branco',
+    },
+  
     // Add more paintings and descriptions as needed
 ];
 
@@ -18,13 +76,12 @@ function getRandomPainting() {
 
     let randomPaintingElement = document.getElementById('randomPainting');
     const paintingDescriptionElement = document.getElementById('paintingDescription');
-    const paintingTextElement = document.getElementById('paintingText');
 
     randomPaintingElement.src = randomPainting.imageSrc;
-    console.log(randomPainting.imageSrc);
     paintingDescriptionElement.innerText = randomPainting.description;
     // Update other elements as needed
 
+    
     // You can also add additional logic to ensure the same painting doesn't appear on the same day
 });
 }
